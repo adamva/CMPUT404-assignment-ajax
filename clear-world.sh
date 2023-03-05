a4a324f9fa032f79fc31062e6da85a2dd03e46e2
Adam Ahmed <avahmed@ualberta.ca>
2023-03-05 16:05:24 -0700

    init script to clear the world

---
+++ clear-world.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+env_file='script.env'
+if [ ! -r $env_file ]; then echo "ERR Could not find $env_file"; exit 1; fi
+source $env_file
+hdr_out=`mktemp`
+url="${APP_URL}/clear"
+
+rsp=`curl -D $hdr_out -X POST -s "$url"`
+e=$?; if [ $e -ne 0 ]; then echo "ERR Failed call to $url"; exit $e; fi
+
+echo "$rsp" | jq >/dev/null 2>&1
+e=$?
+if [ $e -ne 0 ]
+then 
+    echo "ERR Response is not JSON"
+    cat $hdr_out
+    echo "$rsp"
+    exit $e
+else
+    cat $hdr_out
+    echo "$rsp" | jq
+fi
+rm $hdr_out
