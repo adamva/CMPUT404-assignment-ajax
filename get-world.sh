1f0e59fb6b36d6007f03fc4f77f27d3a2c8bab90
Adam Ahmed <avahmed@ualberta.ca>
2023-03-05 16:05:00 -0700

    move url to variable

--- get-world.sh
+++ get-world.sh
@@ -3,9 +3,10 @@ env_file='script.env'
 if [ ! -r $env_file ]; then echo "ERR Could not find $env_file"; exit 1; fi
 source $env_file
 hdr_out=`mktemp`
+url="${APP_URL}/world"
 
-rsp=`curl -D $hdr_out -s "${APP_URL}/world"`
-e=$?; if [ $e -ne 0 ]; then echo "ERR Failed call to ${APP_URL}/world"; exit $e; fi
+rsp=`curl -D $hdr_out -s "$url"`
+e=$?; if [ $e -ne 0 ]; then echo "ERR Failed call to $url"; exit $e; fi
 
 echo "$rsp" | jq >/dev/null 2>&1
 e=$?
